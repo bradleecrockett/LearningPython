4ee99d9c30d4a03614d2850fd1209a6105440482
bradleecrockett <bcrockett@rjuhsd.us>
2017-09-28 21:13:08 -0700

    updated geometry starter file

--- geometry.py
+++ geometry.py
@@ -15,36 +15,36 @@ import math
 def rect_area(length: float, width: float):
     """Returns the area of a rectangle with a given length and width"""
 
-    return
+    return 0
 
 
 def rect_perimeter(length: float, width: float):
     """Returns the perimeter of a rectangle with a given length and width"""
 
-    return
+    return 0
 
 
 def triangle_area(base: float, height: float):
     """Returns the area of a triangle with a given length and width"""
 
-    return
+    return 0
 
 
 def circle_area(radius: float):
     """Returns the area of a circle with a given radius"""
 
-    return
+    return 0
 
 
 def circle_circumference(radius: float):
     """Returns the circumference of a circle  with a given radius"""
 
-    return
+    return 0
 
 
 def calculate_slope(x1: float, y1: float, x2: float, y2: float):
     """Returns the slope between the 2 points (x1, y1) and (x2, y2)"""
 
-    return
+    return 0
 
 
