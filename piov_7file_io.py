096830b148ce1a33091b30be8a8b9b4934e8f3bf
bradleecrockett <bcrockett@rjuhsd.us>
2017-09-28 21:54:42 -0700

    Python In One Video Files
    These files are designed to be stepped through with the debugger.

---
+++ piov_7file_io.py
@@ -0,0 +1,32 @@
+# import the needed modules
+# need the os module for writing and reading files
+import os
+
+
+# FILE I/O -------------
+
+# Overwrite or create a file for writing
+test_file = open("test.txt", "wb")
+
+# Get the file mode used
+print(test_file.mode)
+
+# Get the files name
+print(test_file.name)
+
+# Write text to a file with a newline
+test_file.write(bytes("Write me to the file\n", 'UTF-8'))
+
+# Close the file
+test_file.close()
+
+# Opens a file for reading and writing
+test_file = open("test.txt", "r+")
+
+# Read text from the file
+text_in_file = test_file.read()
+
+print(text_in_file)
+
+# Delete the file uncomment the next line and run it again
+# os.remove("test.txt")
