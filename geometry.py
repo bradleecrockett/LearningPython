ea02ba6a49b332bbb3ee45d6196e2552ae85d9c4
bradleecrockett <bcrockett@rjuhsd.us>
2017-09-28 21:10:50 -0700

    Add files via upload

--- geometry.py
+++ geometry.py
@@ -5,44 +5,46 @@
 # Period:
 #
 # Description:  Define the functions described in comments below.
-#               Functions should all work with sample input/output
-#               as well as other cases
+#               Functions should should pass all tests.  Proper
+#               pep-8 formatting should be used and comments should
+#               be added or updated where appropriate
+#
 import math
 
 
 def rect_area(length: float, width: float):
-    """returns the area of a rectangle with a given length and width"""
+    """Returns the area of a rectangle with a given length and width"""
 
-    return length * width
+    return
 
 
 def rect_perimeter(length: float, width: float):
-    """returns the perimeter of a rectangle with a given length and width"""
+    """Returns the perimeter of a rectangle with a given length and width"""
 
-    return 2 * (length + width)
+    return
 
 
 def triangle_area(base: float, height: float):
-    """returns the area of a triangle with a given length and width"""
+    """Returns the area of a triangle with a given length and width"""
 
-    return base * height / 2
+    return
 
 
 def circle_area(radius: float):
-    """returns the area of a circle with a given radius"""
+    """Returns the area of a circle with a given radius"""
 
-    return radius ** 2 * math.pi
+    return
 
 
 def circle_circumference(radius: float):
-    """returns the circumference of a circle  with a given radius"""
+    """Returns the circumference of a circle  with a given radius"""
 
-    return 2 * math.pi * radius
+    return
 
 
 def calculate_slope(x1: float, y1: float, x2: float, y2: float):
     """Returns the slope between the 2 points (x1, y1) and (x2, y2)"""
 
-    return (y1 - y2) / (x1 - x2)
+    return
 
 
