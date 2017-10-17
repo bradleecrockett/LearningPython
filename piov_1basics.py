cb9dcae2b6a0dfb1a3e68930c2918c8018c46a90
bradleecrockett <bcrockett@rjuhsd.us>
2017-10-16 22:49:43 -0700

    Add files via upload

--- piov_1basics.py
+++ piov_1basics.py
@@ -2,15 +2,18 @@
 # These are called modules
 import math
 
-# Hello world is just one line of code
-# print() outputs data to the screen
-print("Hello World")
-
 '''
 This is a multi-line comment
 You make it with three single quotes
 It helps others read your code
 '''
+
+#This is a single line comment in Python
+
+# Hello world is just one line of code
+# print() outputs data to the screen
+print("Hello World")
+
 
 # A variable is a place to store values
 # Its name is like a label for that value
@@ -31,6 +34,7 @@ name = math.sin(30)
 print(name)
 
 # The arithmetic operators +, -, *, /, %, **, //
+# % modulo operator or remainder operator
 # ** Exponential calculation
 # // Floor Division
 print("5 + 2 =", 5 + 2)
@@ -51,6 +55,7 @@ print("(1 + 2 - 3) * 2 =", (1 + 2 - 3) * 2)
 # A string is a string of characters surrounded by " or '
 # If you must use a " or ' between the same quote escape it with \
 quote = "\"Always remember your unique,"
+number = 5
 
 # A multi-line quote
 multi_line_quote = ''' just
@@ -59,7 +64,13 @@ like everyone else" '''
 print(quote + multi_line_quote)
 
 # To embed a string in output use %s
-print("%s %s %s" % ('I like the quote', quote, multi_line_quote))
+# To embed a float in output use %f
+# To embed an int in output use %d
+print("%s %d %s %f" % ('I like the quote', number, multi_line_quote, number))
+
+# You can also use concatenation but you need to cast the values as a String (str)
+# The result is similar but the accuracy of the float is different
+print("I like the quote " + str(number) + multi_line_quote + str(float(number)))
 
 # To keep from printing newlines use end=""
 print("I don't like ", end="")
