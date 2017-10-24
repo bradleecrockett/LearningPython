8d59eeaf14483b9cf80baf0aa1f77123cccece2f
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2017-10-24 11:23:19 -0700

    Added titles to breaks between examples

--- Playground.py
+++ Playground.py
@@ -1,4 +1,35 @@
 import random
+print("*******Rounding******")
+
+x = 0.0037 / 100
+# These numbers look equal!
+y = 0.000037
+# But they aren't exactly equal!
+print(x)
+print(y)
+if (x == y):
+    print("Equal!")
+else:
+    print("Unequal!")
+
+# ... But they are approximately equal
+# (to five decimal places)!
+if round(x, 5) == round(y, 5):
+    print("Approximately equal!")
+else:
+    print("Not approximately equal!")
+
+
+print("******Break**********")
+
+# This for loop only prints 0 through 4.
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+
+print("******Continue*********")
 
 # we skip back to the top of the for loop
 for i in range(10):
@@ -6,24 +37,19 @@ for i in range(10):
         continue
     print(i)
 
-print("***********************")
 
-size = 5
-for a in range(size):
-    for b in range(size):
-        print(a, b)
-
-
-print("***********************")
+print("*******While*********")
 
 x = 0
 while (x < 10):
     print(x)
     x = x + 1
 
+
 print("after the loop x is: ", x)
 
-print("***********************")
+
+print("*****Loop and a half****")
 
 magic_number = 7
 while (True):
@@ -34,7 +60,8 @@ while (True):
     # Next line is NOT reached if the condition
     print("Try again!")
 
-print("***********************")
+
+print("*****Using Exceptions***")
 
 while(True):
     try:
@@ -48,20 +75,30 @@ while(True):
 
 print("The positive number is: ", num)
 
-# print("***********************")
+
+# print("******What is the output 1******")
 #
-# total = 0
-# for c in range(5):
-#     d = 1
-#     while d <= c:
-#         total = total + d
-#         d += 1
+# size = 5
+# for a in range(size):
+#     for b in range(size):
+#         print(a, b)
 #
-# print(total)
 #
-# print("***********************")
-#
-# # power2 = 1
-# # while(power2 != 20):
-# #     print(power2)
-# #     power2 *= 2
+# # print("******What is the output 2*****")
+# #
+# # total = 0
+# # for c in range(5):
+# #     d = 1
+# #     while d <= c:
+# #         total = total + d
+# #         d += 1
+# #
+# # print(total)
+# #
+# #
+# # print("*****What is the output 3******")
+# #
+# # # power2 = 1
+# # # while(power2 != 20):
+# # #     print(power2)
+# # #     power2 *= 2
