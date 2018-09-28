67089997f0878a14c94749db1dc8adbb85e33d46
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2018-09-28 07:21:56 -0700

    Update try_catch.py

--- try_catch.py
+++ try_catch.py
@@ -1,3 +1,9 @@
+'''
+This file along with test_file.txt is for you to 
+follow along with the video https://youtu.be/NIWwJbo-9_8
+regarding try-except blocks
+'''
+
 try:
     pass
 except Exception:
